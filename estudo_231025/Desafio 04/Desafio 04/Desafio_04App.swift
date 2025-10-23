44a1b168a25f1edc6c9d2be83feb1c3a38b9218d
Lara Cruz <laravsc@hotmail.com>
2025-10-23 11:57:11 -0300

    Add files via upload

---
+++ estudo_231025/Desafio 04/Desafio 04/Desafio_04App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_04App.swift
+//  Desafio 04
+//
+//  Created by Turma01-10 on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
